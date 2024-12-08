26a8eef312a49242481f645f4a40fc3396169c34
Taichi Fujimori <fuji.tai.29@i.softbank.jp>
2024-12-08 16:03:06 +0900

    ~300

--- calendar/src/main.rs
+++ calendar/src/main.rs
@@ -12,6 +12,11 @@ struct Schedule {
   start: NaiveDateTime,
   end: NaiveDateTime
 }
+impl Schedule {
+    fn intersects(&self, other: &Schedule) -> bool {
+      self.start < other.end && other.start < self.end
+    }
+}
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 struct Calendar {
   schedules: Vec<Schedule>,
@@ -82,6 +87,13 @@ fn add_schedule(
     start,
     end,
   };
+  // 重複判定
+  for schedule in &calendar.schedules {
+    if schedule.intersects(&new_schedule){
+      println!("エラー: 予定が重複しています");
+      return;
+    }
+  }
   //予定の追加
   calendar.schedules.push(new_schedule);
 
@@ -92,4 +104,120 @@ fn add_schedule(
     serde_json::to_writer(writer, &calendar).unwrap();
   }
   println!("予定を追加しました。");
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  fn naive_date_time(
+    year: i32,
+    month: u32,
+    day: u32,
+    hour: u32,
+    minute: u32,
+    second: u32,
+  ) -> NaiveDateTime {
+    chrono::NaiveDate::from_ymd_opt(year, month, day)
+      .unwrap()
+      .and_hms_opt(hour, minute, second)
+      .unwrap()
+  }
+
+  #[test]
+  fn test_schedule_intersects_1(){
+    let schedule = Schedule {
+      id: 1,
+      subject: "既存予定１".to_string(),
+      start: naive_date_time(2024, 1, 1, 18, 15, 0),
+      end: naive_date_time(2024, 1, 1, 19, 15, 0),
+    };
+    let new_schedule = Schedule {
+      id: 999,
+      subject: "新規予定".to_string(),
+      start: naive_date_time(2024, 1, 1, 19, 0, 0),
+      end: naive_date_time(2024, 1, 1, 20, 0, 0),
+    };
+    assert!(schedule.intersects(&new_schedule));
+  }
+  #[test]
+  fn test_schedule_intersects_2(){
+    let schedule = Schedule {
+      id: 1,
+      subject: "既存予定１".to_string(),
+      start: naive_date_time(2024, 1, 1, 19, 45, 0),
+      end: naive_date_time(2024, 1, 1, 20, 45, 0),
+    };
+    let new_schedule = Schedule {
+      id: 999,
+      subject: "新規予定".to_string(),
+      start: naive_date_time(2024, 1, 1, 19, 0, 0),
+      end: naive_date_time(2024, 1, 1, 20, 0, 0),
+    };
+    assert!(schedule.intersects(&new_schedule));
+  }
+  #[test]
+  fn test_schedule_intersects_3(){
+    let schedule = Schedule {
+      id: 1,
+      subject: "既存予定１".to_string(),
+      start: naive_date_time(2024, 1, 1, 18, 30, 0),
+      end: naive_date_time(2024, 1, 1, 20, 15, 0),
+    };
+    let new_schedule = Schedule {
+      id: 999,
+      subject: "新規予定".to_string(),
+      start: naive_date_time(2024, 1, 1, 19, 0, 0),
+      end: naive_date_time(2024, 1, 1, 20, 0, 0),
+    };
+    assert!(schedule.intersects(&new_schedule));
+  }
+  #[test]
+  fn test_schedule_intersects_4(){
+    let schedule = Schedule {
+      id: 1,
+      subject: "既存予定１".to_string(),
+      start: naive_date_time(2024, 1, 1, 20, 15, 0),
+      end: naive_date_time(2024, 1, 1, 20, 45, 0),
+    };
+    let new_schedule = Schedule {
+      id: 999,
+      subject: "新規予定".to_string(),
+      start: naive_date_time(2024, 1, 1, 19, 0, 0),
+      end: naive_date_time(2024, 1, 1, 20, 0, 0),
+    };
+    assert!(!schedule.intersects(&new_schedule));
+  }
+  #[test]
+  fn test_schedule_intersects_5(){
+    let schedule = Schedule {
+      id: 1,
+      subject: "既存予定１".to_string(),
+      start: naive_date_time(2024,12, 8, 9, 0, 0),
+      end: naive_date_time(2024, 12, 8, 10, 30, 0),
+    };
+    let new_schedule = Schedule {
+      id: 999,
+      subject: "新規予定".to_string(),
+      start: naive_date_time(2024, 12, 15, 10, 0, 0),
+      end: naive_date_time(2024, 12, 15, 11, 0, 0),
+    };
+    assert!(!schedule.intersects(&new_schedule));
+  }
+  #[test]
+  fn test_schedule_intersects_6(){
+    let schedule= Schedule {
+      id: 6,
+      subject:"既存予定6".to_string(),
+      start: naive_date_time(2024, 1, 1, 19, 10, 0),
+      end: naive_date_time(2024, 1, 1, 19, 45, 0),
+    };
+    let new_schedule = Schedule  {
+      id: 999,
+      subject: "新規予定6".to_string(),
+      start: naive_date_time(2024, 1, 1, 19, 0, 0),
+      end: naive_date_time(2024, 1, 1, 20, 0, 0),
+    };
+    assert!(schedule.intersects(&new_schedule));
+  }
 }
